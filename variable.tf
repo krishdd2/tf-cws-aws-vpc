6858fb6eb68f88ec6da1603a428a5905af1e2ff1
krishdd2 <13781821+krishdd2@users.noreply.github.com>
2022-11-14 23:48:57 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,13 @@
 #variable "aws_access_key" {}
 #variable "aws_secret_key" {}
 #variable "aws_region" {}
-#variable "region" {
-#  description = "Provide the region to deploy the VPC in"
-#  default     = "us-west-2"
-#}
+variable "region" {
+  description = "Provide the region to deploy the VPC in"
+  default     = "us-west-2"
+}
+variable "aws_access_key" {
+  aws_access_key = ""
+  aws_secret_key = " "
+}
+
+  
