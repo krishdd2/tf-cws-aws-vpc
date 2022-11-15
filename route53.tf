a80eb90b901fb7901d9d7c0b0f11282473e06ccf
krishdd2 <13781821+krishdd2@users.noreply.github.com>
2022-11-15 04:16:43 -0500

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,3 +1,23 @@
 resource "aws_route53_zone" "primary_domain" {
   name = "krishnausa.com"
 }
+
+resource "aws_route53_zone" "main" {
+  name = "krishnausa.com"
+}
+
+resource "aws_route53_zone" "dev" {
+  name = "dev.krishnausa.com"
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_route53_record" "dev-ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dev.krishnausa.com"
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.dev.name_servers
+}
